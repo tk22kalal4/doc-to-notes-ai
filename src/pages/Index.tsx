98eb359b393411bc32b93e1cfbf51557b9c76ab7
tk22kalal4 <tarangkalal1234@gmail.com>
2026-07-06 04:48:34 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { FileText, Upload, Scissors, ScanText, Sparkles, X, Download, Image as ImageIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
